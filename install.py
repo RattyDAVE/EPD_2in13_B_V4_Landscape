af98ffc23d7ff874463848a72b09544c6face4e2
RattyDAVE <dave@pucknell.co.uk>
2023-06-18 15:43:14 +0100

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+
+def wget(url, filename):
+    from urequests import get
+    r = get(url)
+    print(r.content)
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+ 
+if not 'lib' in os.listdir(): os.mkdir("/lib")
+wget("https://raw.githubusercontent.com/RattyDAVE/EPD_2in13_B_V4_Landscape/main/EPD_2in13_B_V4_Landscape.py","lib/EPD_2in13_B_V4_Landscape.py"
